09d51a207fc03250191bb1a75ab9c3789f0b284e
Norbert Schneider <norbert@steadybit.com>
2024-12-06 16:24:17 +0100

    feat: support experiment templates as validation type

--- go/advice_kit_api/CHANGELOG.md
+++ go/advice_kit_api/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v1.1.0
+
+- Support experiment templates in validations
+- Update dependencies
+
 ## v1.0.0
 
  - Update dependencies
@@ -7,7 +12,7 @@
 
 ## v0.0.1-beta.6
 
- - add text only validation and there refactoring of the status lifecycle
+ - Add text only validation and there refactoring of the status lifecycle
 
 ## v0.0.1-alpha.3
 ## v0.0.1-alpha.2
--- go/advice_kit_api/build.sh
+++ go/advice_kit_api/build.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
-go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0
-oapi-codegen -config generator-config.yml ../../openapi/spec.yml > advice_kit_api.go
+go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.0
+# Skip first line of output as it contains a warning from oapi-codegen.
+# It can be ignored, as only supported features are used in the spec.
+oapi-codegen -config generator-config.yml ../../openapi/spec.yml | tail -n +2 > advice_kit_api.go
